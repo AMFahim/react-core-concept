cb872d20d13046a1cd31b7671ee898edc62abf93
AMFahim <amfahim2019@gmail.com>
2021-02-23 19:22:55 +0600

    react-biggenr-concept

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name : "Fahim",
+    Job : "Web Developer"
+  }
+  var person2 = {
+    name : "Rahim",
+    Job : "Web Developer"
+  }
+  var style ={
+    color: "salmon",
+    backgroundColor: "black"
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +21,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <h1 className="" style={style}>My heading: {person.name + " " + person.Job} </h1>
+        <h2 style ={{backgroundColor:"cyan"}}>Developer: {person2.name + " " + person.Job}</h2>
+        <p>my first paragraph</p>
       </header>
     </div>
   );
