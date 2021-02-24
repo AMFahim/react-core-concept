c3719d133dfb404366012105ee457f891eab7af8
AMFahim <amfahim2019@gmail.com>
2021-02-24 17:24:18 +0600

    Component state hook and set state method

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const lovers = ["Rahat", "Yakub", "Rakib"]
+  const lovers = ["Rahat", "Yakub", "Rakib", "Faysal","Azim","Mashraful"]
   const products = [
     { name: "Photoshop", price: "$90.99" },
     { name: "illustrator", price: "$60.99" },
     {name: "PDF Reader", price: "$6.99"}
 
   ]
+
   return (
     <div className="App">
       <header className="App-header">
         <p>I am react person</p>
+        <Counter></Counter>
+        <ul>
+           {
+             lovers.map(name => <li>{name}</li>)
+           }
+           {
+             products.map(product=><li>{product.name}</li>)
+           }
+        </ul>
+        {
+          products.map(pd => <Product product={pd}></Product>)
+        }
         <Product product={products[0]}></Product>
         <Product product={products[1]} ></Product>
         <Product product={products[2]}></Product>
@@ -24,6 +38,21 @@ function App() {
     </div>
   );
 }
+
+function Counter() {
+  const [count, setCount] = useState(10);
+  // const handleIncrease = () => setCount(count + 1);
+  // const handleDecrease = () => setCount(count - 1);
+  
+  return(
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  )
+}
+
 function Person(props) {
   const personStyle = {
     border: "2px solid red",
@@ -33,6 +62,7 @@ function Person(props) {
     height: "200px",
     width: "200px",
   }
+ 
   return (
     <div style={personStyle}>
       <h1>Name: {props.name}</h1>
